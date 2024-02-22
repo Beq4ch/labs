2a2862e249fd85f121cc3a08d8f12754b8a2877d
Dima M <frost.hjh@gmail.com>
2024-02-22 20:25:54 +0300

    Add files

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+struct Queue {
+    Node* front;
+    Node* rare;
+};
+
+Queue* createQueue() {
+    
+}
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
---
+++ LabTest/LabTest.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+class Stack {
+private:
+    int* arr;
+    int top;
+    int size;
+public:
+
+    Stack() {
+        arr = nullptr;
+        top = -1;
+        size = 0;
+    }
+
+    Stack(int n) {
+        arr = new int[n];
+        size = n;
+        top = -1;
+    }
+
+    ~Stack() {
+        delete[] arr;
+    }
+    
+
+
+
+};
+
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
