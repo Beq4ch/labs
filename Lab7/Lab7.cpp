a3e417d84d6be6c2a8117281a3ce4e3a5357a50d
Dima M <frost.hjh@gmail.com>
2024-05-20 00:05:36 +0300

    Update Lab7

--- Lab7/Lab7.cpp
+++ Lab7/Lab7.cpp
@@ -1,122 +1,223 @@
 ﻿#include <iostream>
 using namespace std;
 
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
 enum Order { Prefix, Infix, Postfix };
 
 class Tree {
 private:
-    struct Node {
-        int data;
-        Node* left;
-        Node* right;
-        Node(int value) : data(value), left(nullptr), right(nullptr) {}
-    };
-
-    Node* root;
     int size;
+    Node* root;
 
-    void clearNode(Node* node) {
-        if (node != nullptr) {
-            clearNode(node->left);
-            clearNode(node->right);
-            delete node;
-        }
-    }
-
-    int countNodes(Node* node) const {
-        if (node == nullptr) return 0;
-        return 1 + countNodes(node->left) + countNodes(node->right);
-    }
-
-    void fillArray(Node* node, int*& array, int& index, Order order) {
-        if (node == nullptr) return;
+    void toArrayHelper(Node* node, int*& array, int& index, Order order) {
+        if (!node) return;
         if (order == Prefix) array[index++] = node->data;
-        fillArray(node->left, array, index, order);
+        toArrayHelper(node->left, array, index, order);
         if (order == Infix) array[index++] = node->data;
-        fillArray(node->right, array, index, order);
+        toArrayHelper(node->right, array, index, order);
         if (order == Postfix) array[index++] = node->data;
     }
 
-    Node* findNode(Node* node, int value) {
-        while (node != nullptr && node->data != value) {
-            if (value < node->data) node = node->left;
-            else node = node->right;
+    Node* rotateLeft(Node* node) {
+        Node* newRoot = node->right;
+        node->right = newRoot->left;
+        newRoot->left = node;
+        return newRoot;
+    }
+
+    Node* rotateRight(Node* node) {
+        Node* newRoot = node->left;
+        node->left = newRoot->right;
+        newRoot->right = node;
+        return newRoot;
+    }
+
+    Node* balanceHelper(Node* node) {
+        if (!node) return nullptr;
+
+        node->left = balanceHelper(node->left);
+        node->right = balanceHelper(node->right);
+
+        int leftDepth = depth(node->left);
+        int rightDepth = depth(node->right);
+
+        if (leftDepth > rightDepth + 1) {
+            if (depth(node->left->left) < depth(node->left->right)) {
+                node->left = rotateLeft(node->left);
+            }
+            node = rotateRight(node);
         }
+        else if (rightDepth > leftDepth + 1) {
+            if (depth(node->right->right) < depth(node->right->left)) {
+                node->right = rotateRight(node->right);
+            }
+            node = rotateLeft(node);
+        }
+
         return node;
     }
 
-    void rotateLeft(Node*& node) {
-        Node* rightChild = node->right;
-        node->right = rightChild->left;
-        rightChild->left = node;
-        node = rightChild;
+    int depth(Node* node) {
+        if (!node) return 0;
+        int leftDepth = depth(node->left);
+        int rightDepth = depth(node->right);
+        return max(leftDepth, rightDepth) + 1;
     }
 
-    void rotateRight(Node*& node) {
-        Node* leftChild = node->left;
-        node->left = leftChild->right;
-        leftChild->right = node;
-        node = leftChild;
-    }
-
-    void storeBSTNodes(Node* root, Node** nodes, int& index) {
-        if (root == nullptr) return;
-        storeBSTNodes(root->left, nodes, index);
-        nodes[index++] = root;
-        storeBSTNodes(root->right, nodes, index);
-    }
-
-    Node* buildTreeUtil(Node** nodes, int start, int end) {
-        if (start > end) return nullptr;
-        int mid = (start + end) / 2;
-        Node* node = nodes[mid];
-        node->left = buildTreeUtil(nodes, start, mid - 1);
-        node->right = buildTreeUtil(nodes, mid + 1, end);
-        return node;
-    }
-
-    Node* balanceBST(Node* root) {
-        int nodeCount = countNodes(root);
-        Node** nodes = new Node * [nodeCount];
-        int index = 0;
-        storeBSTNodes(root, nodes, index);
-        Node* balancedRoot = buildTreeUtil(nodes, 0, nodeCount - 1);
-        delete[] nodes;
-        return balancedRoot;
+    Node* findNode(Node* node, int value) {
+        if (!node || node->data == value) return node;
+        if (value < node->data) return findNode(node->left, value);
+        return findNode(node->right, value);
     }
 
 public:
-    Tree() : root(nullptr), size(0) {}
+
+    Tree() {
+        size = 0;
+        root = nullptr;
+    }
 
     ~Tree() {
-        clearNode(root);
+        clear();
     }
 
     void add(int value) {
         Node* newNode = new Node(value);
+
         if (!root) {
             root = newNode;
+            size++;
+            return;
         }
-        else {
-            Node* current = root;
-            Node* parent = nullptr;
-            while (current) {
-                parent = current;
-                if (value < current->data) current = current->left;
-                else if (value > current->data) current = current->right;
-                else return; // No duplicate values
+
+        Node* curr = root;
+        while (true) {
+            if (value < curr->data) {
+                if (!curr->left) {
+                    curr->left = newNode;
+                    size++;
+                    break;
+                }
+                else {
+                    curr = curr->left;
+                }
             }
-            if (value < parent->data) parent->left = newNode;
-            else parent->right = newNode;
+            else if (value > curr->data) {
+                if (!curr->right) {
+                    curr->right = newNode;
+                    size++;
+                    break;
+                }
+                else {
+                    curr = curr->right;
+                }
+            }
+            else {
+                delete newNode;
+                break;
+            }
         }
-        size++;
+    }
+
+    bool contains(int value) {
+        Node* curr = root;
+        while (curr) {
+            if (value < curr->data) {
+                curr = curr->left;
+            }
+            else if (value > curr->data) {
+                curr = curr->right;
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void removeHelper(Node*& node, int value) {
+        if (!node) return;  // Узел не найден
+
+        if (value < node->data) {
+            removeHelper(node->left, value);
+        }
+        else if (value > node->data) {
+            removeHelper(node->right, value);
+        }
+        else {  // Найден узел, который нужно удалить
+            if (!node->left && !node->right) {  // Узел является листом
+                delete node;
+                node = nullptr;
+                size--;
+            }
+            else if (node->left && !node->right) {  // Узел имеет только левого ребенка
+                Node* temp = node;
+                node = node->left;
+                delete temp;
+                size--;
+            }
+            else if (!node->left && node->right) {  // Узел имеет только правого ребенка
+                Node* temp = node;
+                node = node->right;
+                delete temp;
+                size--;
+            }
+            else {  // Узел имеет двух детей
+                Node* minNode = node->right;
+                Node* parentOfMinNode = node;
+                if (!minNode->left) {  // Минимальный узел находится непосредственно справа
+                    node->data = minNode->data;
+                    node->right = minNode->right;  // Подвесим правое поддерево минимального узла к текущему узлу
+                    delete minNode;
+                }
+                else {
+                    while (minNode->left) {  // Находим минимальный узел в правом поддереве
+                        parentOfMinNode = minNode;
+                        minNode = minNode->left;
+                    }
+                    node->data = minNode->data;  // Заменяем данные узла на минимальные из правого поддерева
+                    removeHelper(parentOfMinNode->left, minNode->data);  // Удаляем минимальный узел
+                }
+            }
+        }
+    }
+
+    void remove(int value) {
+        removeHelper(root, value);
+    }
+
+    int count() {
+        return size;
+    }
+
+    void clearNode(Node* node) {
+        if (!node) return;
+        clearNode(node->left);
+        clearNode(node->right);
+        delete node;
+    }
+
+    void clear() {
+        clearNode(root);
+        root = nullptr;
+        size = 0;
     }
 
     int* ToArray(Order order) {
-        int nodeCount = countNodes(root);
-        int* array = new int[nodeCount];
+        int* array = new int[size];
         int index = 0;
-        fillArray(root, array, index, order);
+        toArrayHelper(root, array, index, order);
         return array;
     }
 
@@ -125,32 +226,47 @@ public:
     }
 
     void ToLeft(int value) {
-        Node* target = findNode(root, value);
-        if (target && target->right) rotateLeft(target);
+        Node* parent = nullptr;
+        Node* node = root;
+
+        while (node && node->data != value) {
+            parent = node;
+            if (value < node->data) node = node->left;
+            else node = node->right;
+        }
+
+        if (!node || !node->right) return;
+
+        if (!parent) root = rotateLeft(node);
+        else if (parent->left == node) parent->left = rotateLeft(node);
+        else parent->right = rotateLeft(node);
     }
 
     void ToRight(int value) {
-        Node* target = findNode(root, value);
-        if (target && target->left) rotateRight(target);
+        Node* parent = nullptr;
+        Node* node = root;
+
+        while (node && node->data != value) {
+            parent = node;
+            if (value < node->data) node = node->left;
+            else node = node->right;
+        }
+
+        if (!node || !node->left) return;
+
+        if (!parent) root = rotateRight(node);
+        else if (parent->left == node) parent->left = rotateRight(node);
+        else parent->right = rotateRight(node);
     }
 
     void Balance() {
-        root = balanceBST(root);
-    }
-
-    int count() {
-        return size;
-    }
-
-    void clear() {
-        clearNode(root);
-        root = nullptr;
-        size = 0;
+        root = balanceHelper(root);
     }
 };
 
 int main() {
     Tree t;
+
     string choice;
     int data;
 
@@ -160,11 +276,28 @@ int main() {
             cin >> data;
             t.add(data);
         }
+        else if (choice == "remove") {
+            cin >> data;
+            t.remove(data);
+        }
         else if (choice == "clear") {
             t.clear();
         }
         else if (choice == "count") {
             cout << t.count() << endl;
+        }
+        else if (choice == "toarray") {
+            string orderStr;
+            cin >> orderStr;
+            Order order = Infix;
+            if (orderStr == "prefix") order = Prefix;
+            else if (orderStr == "postfix") order = Postfix;
+            int* arr = t.ToArray(order);
+            for (int i = 0; i < t.count(); i++) {
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+            delete[] arr;
         }
         else if (choice == "toleft") {
             cin >> data;
@@ -176,26 +309,6 @@ int main() {
         }
         else if (choice == "balance") {
             t.Balance();
-        }
-        else if (choice == "toarray") {
-            string order;
-            cin >> order;
-            int* array = nullptr;
-            int size = t.count();
-            if (order == "infix") {
-                array = t.ToArray(Infix);
-            } else if (order == "prefix") {
-                array = t.ToArray(Prefix);
-            } else if (order == "postfix") {
-                array = t.ToArray(Postfix);
-            }
-            if (array != nullptr) {
-                for (int i = 0; i < size; i++) {
-                    cout << array[i] << " ";
-                }
-                cout << endl;
-                delete[] array; // Clear memory
-            }
         }
         else if (choice == "stop") {
             break;
